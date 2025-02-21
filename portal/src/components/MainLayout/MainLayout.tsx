91f04cb2e7ac28e2630fc548d49b2653a5c7c898
Mahmoud Ibrahim <mahmoudm.ibrahim53@gmail.com>
2025-02-21 16:15:56 +0200

    [FRONTEND][ADD]: Add main layout and nav bar

--- portal/src/components/MainLayout/MainLayout.tsx
+++ portal/src/components/MainLayout/MainLayout.tsx
@@ -1,6 +1,37 @@
+// import './MainLayout.scss';
+// import { FC } from 'react';
+// interface MyProps {
+//   children?: React.ReactNode;
+// }
+// export const MainLayout: FC<MyProps> = ({ children }) => {
+//   return <div className="main-layout-wrapper">{children}</div>;
+// };
+
 import './MainLayout.scss';
 import { FC } from 'react';
+interface MyProps {
+  children?: React.ReactNode;
+}
+import { Layout } from 'antd';
 
-export const MainLayout: FC = () => {
-  return <div></div>;
+export const MainLayout: FC<MyProps> = ({ children }) => {
+  return (
+    <Layout className="main-layout-wrapper">
+      <Layout.Header className="main-layout-header">
+        <div className="home-menu-item">
+          <div>Home</div>
+        </div>
+        <div className="auth-menu-wrapper">
+          <div>Sign Up</div>
+          <div>Sign In</div>
+        </div>
+      </Layout.Header>
+      <Layout.Content className="main-layout-content">
+        <div className="main-layout-content-elements-wrapper">{children}</div>
+      </Layout.Content>
+      <Layout.Footer className="main-layout-footer">
+        Mahmoud Ibrahim ©{new Date().getFullYear()} Created by Mahmoud Ibrahim
+      </Layout.Footer>
+    </Layout>
+  );
 };
