d07e2e9d3cbb614f3ad93882d6693778f954b16f
Mahmoud Ibrahim <mahmoudm.ibrahim53@gmail.com>
2025-02-21 14:31:11 +0200

    [FRONTEND][ADD]: Add sign up form

--- portal/src/pages/SignUp/SignUp.tsx
+++ portal/src/pages/SignUp/SignUp.tsx
@@ -1,6 +1,161 @@
+import { Button, Checkbox, Form, Input } from 'antd';
 import './SignUp.scss';
 import { FC } from 'react';
+import { ValidateErrorEntity } from 'rc-field-form/lib/interface';
+interface SignUpFormValues {
+  username: string;
+  password: string;
+  remember: boolean;
+}
 
 export const SignUp: FC = () => {
-  return <div>Welcome to My Sign Up Page</div>;
+  const onFinish = (values: SignUpFormValues) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: ValidateErrorEntity<SignUpFormValues>) => {
+    console.log('Failed:', errorInfo);
+  };
+  return (
+    <div className="sign-up-page-wrapper">
+      <h1>Welcome to My Site</h1>
+      <Form
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        className="sign-up-form"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <div className="sign-up-form-inputs-wrapper">
+          <div className="sign-up-form-item-wrapper">
+            <Form.Item
+              label="First Name"
+              name="first_name"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your first name!',
+                },
+              ]}
+            >
+              <Input className="sign-up-form-input" />
+            </Form.Item>
+          </div>
+          <div className="sign-up-form-item-wrapper">
+            <Form.Item
+              label="Last Name"
+              name="last_name"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your last name!',
+                },
+              ]}
+            >
+              <Input className="sign-up-form-input" />
+            </Form.Item>
+          </div>
+          <div className="sign-up-form-item-wrapper">
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Email!',
+                },
+              ]}
+            >
+              <Input className="sign-up-form-input" />
+            </Form.Item>
+          </div>
+          <div className="sign-up-form-item-wrapper">
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your username!',
+                },
+              ]}
+            >
+              <Input className="sign-up-form-input" />
+            </Form.Item>
+          </div>
+          <div className="sign-up-address-wrapper">
+            <Form.Item
+              label="Address"
+              name="address"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Address!',
+                },
+              ]}
+            >
+              <Input className="sign-up-form-input" />
+            </Form.Item>
+          </div>
+          <div className="sign-up-form-item-wrapper">
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+            >
+              <Input.Password className="sign-up-form-input" />
+            </Form.Item>
+          </div>
+          <div className="sign-up-form-item-wrapper">
+            <Form.Item
+              label="Confirm Password"
+              name="confirm_password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password again!',
+                },
+              ]}
+            >
+              <Input.Password className="sign-up-form-input" />
+            </Form.Item>
+          </div>
+        </div>
+        <Form.Item
+          className="sign-up-form-item"
+          name="agree_to_terms"
+          valuePropName="checked"
+          label={null}
+          rules={[
+            {
+              required: true,
+              message: 'You must agree to terms & conditions!',
+            },
+          ]}
+        >
+          <Checkbox className="sign-up-form-checkbox">
+            I have read <a>Terms & Conditions</a> and agree
+          </Checkbox>
+        </Form.Item>
+
+        <Form.Item className="sign-up-form-item" label={null}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="sign-up-form-submit-button"
+          >
+            SignUp
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
